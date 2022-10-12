230a6fc2e29c0889da1e5d38dbf4ff75bbbe2ea4
Niklas Solomon <niklassolomon@gmail.com>
2022-10-11 21:39:52 -0700

    create user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,7 +41,12 @@ const userController = {
                 console.log(err);
                 res.status(500).json(err);
             });
-    }
+    },
+    createUser({ body }, res) {
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
