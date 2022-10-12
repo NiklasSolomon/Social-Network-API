7458489d8f758612ee2cf8f290d56b15cc506cde
Niklas Solomon <niklassolomon@gmail.com>
2022-10-11 21:46:37 -0700

    delete user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,7 +32,7 @@ const userController = {
             select('-__v')
             .then(userData => {
                 if(!userData) {
-                    res.status(404).json({ message: 'Invalid ID'});
+                    res.status(404).json({ message: 'Invalid ID' });
                     return;
                 }
                 res.json(userData)
@@ -51,13 +51,24 @@ const userController = {
         User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
             .then(userData => {
                 if(!userData) {
-                    res.status(404).json({ message: 'Invalid ID'});
+                    res.status(404).json({ message: 'Invalid ID' });
                     return;
                 }
                 res.json(userData);
             })
             .catch(err => res.status(400).json(err));
-    }
+    },
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(userData => {
+                if(!userData) {
+                    res.status(404).json({ message: 'Invalid ID' });
+                    return;
+                }
+                res.json(userData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
